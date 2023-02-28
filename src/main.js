110df6b95de72793fffc14beb7df3d9a6e74d678
harmen.dev <hello@harmen.dev>
2023-02-28 21:39:57 +0100

    Add public path to router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
 import App from './App.vue'
-
+import { publicPath } from '../vue.config'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
+  base: publicPath,
   routes: [
     {
       path: '/',
