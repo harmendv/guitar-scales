043edc88840a7997e4771760c90ca37eee57bba7
harmendv <harmendv@gmail.com>
2024-02-10 22:11:23 +0100

    Add VueChords

---
+++ src/components/VueChords.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="vue-chords">
+        <lv-flex direction='row' fill>
+            <lv-card v-for="chord in chords" inline>
+              <strong>{{ chord.degree }}.</strong> {{ chord.note }}{{ chord.chord}}
+            </lv-card>
+        </lv-flex>
+
+    </div>
+</template>
+
+<script>
+import { LvFlex, LvCard } from '@libvue/core';
+export default {
+    components: {
+        LvFlex,
+        LvCard
+    },
+    props: {
+        chords: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
+
+<style lang="scss"></style>
