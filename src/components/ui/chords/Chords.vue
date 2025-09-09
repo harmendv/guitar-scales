1e916cb5a2c0ce6b5409068bdef16fefa4691f53
harmendv <harmendv@gmail.com>
2025-09-09 13:43:19 +0200

    Add Logo component and update Chords.vue for improved chord display

--- src/components/ui/chords/Chords.vue
+++ src/components/ui/chords/Chords.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="flex flex-wrap w-full gap-6">
-        <div class="border p-2 cursor-pointer grow rounded rounded-md" v-for="(chord, index) in chords" :class="{'border-indigo-700 bg-indigo-100 dark:bg-indigo-900': active === index + 1}" @click="onClickChord(index)">
-            <div class="bg-slate-100 dark:bg-slate-800 mb-2 rounded" :class="{ '!bg-indigo-500 !dark:bg-indigo-500 text-white': active === index + 1 }">{{ chord.degree }}</div>
+        <div class="border p-2 cursor-pointer grow rounded-md" v-for="(chord, index) in chords" :class="{'border-indigo-700 bg-indigo-100 dark:bg-indigo-900': active === index + 1}" @click="onClickChord(index)">
             <div class="font-bold">{{ chord.note }}<sup>{{ chord.chord }}</sup></div>
         </div>
     </div>
